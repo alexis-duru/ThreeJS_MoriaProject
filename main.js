afda234af02f95a3bfc3d1f83e6fb5cefb73631c
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2022-06-16 18:31:55 +0200

    up
    up

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css';
 import './experience/index.js';
 import './experience/gsap/app.js';
-import './experience/gsap/letters.js';
+import './experience/textanimation/letters.js';
 // import './experience/Sound/Amplitude.js';
 
 
